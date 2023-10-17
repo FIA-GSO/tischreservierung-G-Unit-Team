3d733560910f77b14cacf4e3fdb2e59b3d6c84d0
Mikhail Zhivoderov <stueckholz@hotmail.com>
2023-10-17 14:28:55 +0200

    Added method
    mocked a method to get all reservations

--- service/server.py
+++ service/server.py
@@ -1,7 +1,20 @@
 from flask import Flask
 
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+import sqlite3
 
+app = Flask(__name__)
+DATABASE = 'buchungssystem.sqlite'
+
+
+@app.route('/reservierungen', methods=['GET'])
+def get_reservierungen():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM reservierungen')
+    reservierungen = cursor.fetchall()
+    conn.close()
+    return jsonify({'reservierungen': reservierungen})
 
 @app.route("/")
 def hello_world():
