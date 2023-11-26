1f3a20d9ee0111f9cba4b7a7b3d07daaabad0201
Mikhail Zhivoderov <stueckholz@hotmail.com>
2023-11-26 17:55:05 +0100

    init funktio add reservierung and other functions to get tables etc.

--- service/server.py
+++ service/server.py
@@ -1,3 +1,5 @@
+import traceback
+
 from flask import Flask
 
 from flask import Flask, request, jsonify
@@ -7,7 +9,22 @@ app = Flask(__name__)
 DATABASE = 'buchungssystem.sqlite'
 
 
-@app.route('/reservierungen', methods=['GET'])
+def init_db():
+    print("Initializing database")
+    try:
+
+        with sqlite3.connect(DATABASE) as con:
+
+            with open("create_buchungssystem.sql", "r") as f:
+                con.executescript(f.read())
+
+    except Exception as e:
+
+        print(f"Error initializing database: {e}")
+        traceback.print_exc()
+
+
+@app.route('/reservierungen/all', methods=['GET'])
 def get_reservierungen():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
@@ -16,6 +33,32 @@ def get_reservierungen():
     conn.close()
     return jsonify({'reservierungen': reservierungen})
 
+
+# get all tische from database
+@app.route('/tische/all', methods=['GET'])
+def get_tische():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tische')
+    tische = cursor.fetchall()
+    conn.close()
+    return jsonify({'tische': tische})
+
+
+# show all reserved tables ordered by time
+@app.route('/reservierungen/reserved', methods=['GET'])
+def get_reserved_tische():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT tischnummer, zeitpunkt FROM reservierungen ORDER BY zeitpunkt ASC')
+    tische = cursor.fetchall()
+    conn.close()
+    return jsonify({'tische': tische})
+
+
 @app.route("/")
 def hello_world():
+    init_db()
+    app.logger.info("Database connection successful")
+    print("App started")
     return "<p>Hello, World!</p>"
